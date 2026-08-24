d036d986c65ed553b8e95f52ef42993fb4f21460
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 15:28:42 -0400

    Tema 03: Secuencial parte final

--- 03-Secuencial/Program.cs
+++ 03-Secuencial/Program.cs
@@ -7,7 +7,13 @@ class Program
         int numero1 = 10;
         int numero2 = 5;
         int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        double division = (double)numero1 / numero2;
 
-        Console.WriteLine("La suma es: " + suma);
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
     }
 }
