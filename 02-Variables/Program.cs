1dc6210e80fc2ee3b1e643e0945bd49127f32483
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 14:46:35 -0400

    Tema 02: Variables parte 1

---
+++ 02-Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int edad = 20;
+        string nombre = "Haniel";
+
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Edad: " + edad);
+    }
+}
