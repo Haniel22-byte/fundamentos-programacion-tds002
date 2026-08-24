d910a5ac35ae7ed37dea0423bdaf55cd597e166a
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 17:40:36 -0400

    Tema 07: Ciclos parte 1

---
+++ 07-Ciclos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ciclo for: imprime números del 1 al 5
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Número: " + i);
+        }
+    }
+}
