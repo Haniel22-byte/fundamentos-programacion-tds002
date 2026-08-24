db50f030f580566f65f7f44a0fa9096ee5f43774
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 15:14:48 -0400

    Tema 02: Variables parte final

--- 02-Variables/Program.cs
+++ 02-Variables/Program.cs
@@ -10,9 +10,14 @@ class Program
         double altura = 1.75;
         bool estudiante = true;
 
+        char inicial = 'H';
+        decimal salario = 1500.50m;
+
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("Edad: " + edad);
         Console.WriteLine("Altura: " + altura);
         Console.WriteLine("¿Es estudiante?: " + estudiante);
+        Console.WriteLine("Inicial: " + inicial);
+        Console.WriteLine("Salario: " + salario);
     }
 }
