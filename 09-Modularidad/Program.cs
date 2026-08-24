56cb7bd0cf7100f58ea13614af69c41b7cee1f05
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 19:36:28 -0400

    Tema 09: Modularidad parte 2

--- 09-Modularidad/Program.cs
+++ 09-Modularidad/Program.cs
@@ -5,10 +5,17 @@ class Program
     static void Main(string[] args)
     {
         MostrarMensaje();
+        MostrarSuma(5, 7);
     }
 
     static void MostrarMensaje()
     {
         Console.WriteLine("Bienvenido al módulo de modularidad.");
+    }
+
+    static void MostrarSuma(int a, int b)
+    {
+        int resultado = a + b;
+        Console.WriteLine("La suma es: " + resultado);
     }
 }
