3478b43fee362b48cfa26818806c234846bd9f55
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 19:35:19 -0400

    Tema 09: Modularidad parte 1

---
+++ 09-Modularidad/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MostrarMensaje();
+    }
+
+    static void MostrarMensaje()
+    {
+        Console.WriteLine("Bienvenido al módulo de modularidad.");
+    }
+}
