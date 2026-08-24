92ff9c442e31ae1f319509e5f26509b613c4eba5
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 19:21:18 -0400

    Tema 08: Arreglos parte 2

--- 08-Metodos/Program.cs
+++ 08-Metodos/Program.cs
@@ -9,5 +9,20 @@ class Program
 
         Console.WriteLine("Primer número: " + numeros[0]);
         Console.WriteLine("Último número: " + numeros[4]);
+
+        // Recorrido con for
+        Console.WriteLine("\nRecorriendo el arreglo:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine("Elemento en posición " + i + ": " + numeros[i]);
+        }
+
+        // Arreglo de cadenas
+        string[] nombres = { "Haniel", "Carlos", "Ana" };
+        Console.WriteLine("\nLista de nombres:");
+        foreach (string nombre in nombres)
+        {
+            Console.WriteLine(nombre);
+        }
     }
 }
