baaaff77be40357b9a54f50a746830e8bdc03768
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 18:20:15 -0400

    Tema 08: Arreglos parte 1

---
+++ 08-Metodos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Arreglo de enteros
+        int[] numeros = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Primer número: " + numeros[0]);
+        Console.WriteLine("Último número: " + numeros[4]);
+    }
+}
