aa48a94bb3945bb7cedb981292e9d1c6c4ff66e1
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 15:59:43 -0400

    Tema 04: Cálculos y formato parte 1

---
+++ 04-CalculosFormato/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double precio = 120.50;
+        double cantidad = 3;
+        double total = precio * cantidad;
+
+        Console.WriteLine("El total es: " + total);
+    }
+}
