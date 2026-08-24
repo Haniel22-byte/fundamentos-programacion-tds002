e413b3d70f1c826e6c55828e0b855449862ff253
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 16:20:21 -0400

    Tema 06: Decisiones parte 1

--- 06-Decisiones/Program.cs
+++ 06-Decisiones/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int edad = 20;
+        bool tieneLicencia = true;
+
+        if (edad >= 18 && tieneLicencia)
+        {
+            Console.WriteLine("Puedes conducir.");
+        }
+        else
+        {
+            Console.WriteLine("No puedes conducir.");
+        }
+    }
+}
