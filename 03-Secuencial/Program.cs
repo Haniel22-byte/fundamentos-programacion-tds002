6753ad07f7572c37f74fc5e2fbe7b104d80d44ce
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 15:26:52 -0400

    Tema 03: Secuencial parte 1

---
+++ 03-Secuencial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero1 = 10;
+        int numero2 = 5;
+        int suma = numero1 + numero2;
+
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
