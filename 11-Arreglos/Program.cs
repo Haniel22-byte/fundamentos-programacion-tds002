6b6f7a1103c4fa250f4d99a0dc47501aeea48f44
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 20:12:12 -0400

    Tema 11: Clases y Objetos parte 1

---
+++ 11-Arreglos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Persona
+{
+    public string Nombre;
+    public int Edad;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona p1 = new Persona();
+        p1.Nombre = "Haniel";
+        p1.Edad = 20;
+
+        Console.WriteLine("Nombre: " + p1.Nombre);
+        Console.WriteLine("Edad: " + p1.Edad);
+    }
+}
