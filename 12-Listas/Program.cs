5ab7aed22cc8d0369bb9a041d4347fb604fcab58
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 20:28:57 -0400

    Tema 12: Herencia parte 1

---
+++ 12-Listas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Persona
+{
+    public string Nombre;
+    public int Edad;
+
+    public void Saludar()
+    {
+        Console.WriteLine("Hola, soy " + Nombre);
+    }
+}
+
+class Estudiante : Persona
+{
+    public string Carrera;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estudiante e1 = new Estudiante();
+        e1.Nombre = "Haniel";
+        e1.Edad = 20;
+        e1.Carrera = "Informática";
+
+        e1.Saludar();
+        Console.WriteLine("Carrera: " + e1.Carrera);
+    }
+}
