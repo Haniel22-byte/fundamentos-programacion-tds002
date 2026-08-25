697b9ba18c7b0953e29aad8b7b62b5192dbf7942
Haniel22-byte <20260970@itla.edu.do>
2026-08-24 20:13:45 -0400

    Tema 11: Clases y Objetos parte 2

--- 11-Arreglos/Program.cs
+++ 11-Arreglos/Program.cs
@@ -4,15 +4,20 @@ class Persona
 {
     public string Nombre;
     public int Edad;
+
+    // Constructor
+    public Persona(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad = edad;
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Persona p1 = new Persona();
-        p1.Nombre = "Haniel";
-        p1.Edad = 20;
+        Persona p1 = new Persona("Haniel", 20);
 
         Console.WriteLine("Nombre: " + p1.Nombre);
         Console.WriteLine("Edad: " + p1.Edad);
